f26fe3c945e523e6abac50645f17c2afd80231d5
Катя <lezginakata@gmail.com>
2025-10-04 13:51:51 +0300

    чтоугодно

--- code/lab02/ex01.py
+++ code/lab02/ex01.py
@@ -7,3 +7,13 @@ print(min_max([42]))
 print(min_max([-5, -2, -9]))
 print(min_max([]))
 print(min_max([1.5, 2, 2.0, -3.1]))
+
+def unique_sorted(nums: list[float | int]) -> list[float | int]:
+    return sorted(set(nums))
+print(unique_sorted([3, 1, 2, 1, 3]))
+print(unique_sorted([]))
+print(unique_sorted([-1, -1, 0, 2, 2]))
+print(unique_sorted([1.0, 1, 2.5, 2.5, 0]))
+
+def flatten(mat: list[list | tuple]) -> list:
+    
