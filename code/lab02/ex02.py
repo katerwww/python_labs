8abe52d78cce8657ee1152eb25e9e0138b7ef51d
Катя <lezginakata@gmail.com>
2025-10-15 21:28:45 +0300

    чтоугодно

---
+++ code/lab02/ex02.py
@@ -0,0 +1,45 @@
+def transpose(mat: list[list[float | int]]) -> list[list]:
+    if not mat:
+        return []
+    rv_l = len(mat[0])
+    for rv in mat:
+        if len(rv) != rv_l:
+            return ValueError
+    return [[mat[j][i] for j in range(len(mat))] for i in range(rv_l)]
+
+print(transpose([[1, 2, 3]]))
+print(transpose([[1], [2], [3]]))
+print(transpose([[1, 2], [3, 4]]))
+print(transpose([]))
+print(transpose([[1, 2], [3]]))
+
+def row_sums(mat: list[list[float | int]]) -> list[float]:
+    if not mat:
+        return []
+    
+    row_length = len(mat[0])
+    for row in mat:
+        if len(row) != row_length:
+            return ValueError
+    
+    return [sum(row) for row in mat]
+print(row_sums([[1, 2, 3], [4, 5, 6]]))
+print(row_sums([[-1, 1], [10, -10]]))
+print(row_sums([[0, 0], [0, 0]]))
+print(row_sums([[1, 2], [3]]))
+
+def col_sums(mat: list[list[float | int]]) -> list[float]:
+    if not mat:
+        return []
+    
+    row_length = len(mat[0])
+    for row in mat:
+        if len(row) != row_length:
+            return ValueError
+    
+    return [sum(mat[i][j] for i in range(len(mat))) for j in range(row_length)]
+
+print(col_sums([[1, 2, 3], [4, 5, 6]]))
+print(col_sums([[-1, 1], [10, -10]]))
+print(col_sums([[0, 0], [0, 0]]))
+print(col_sums([[1, 2], [3]]))
